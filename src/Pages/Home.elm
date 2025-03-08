abc40016073fe968ce3ec2ad04c8b86af25bc1d9
Simon Tenggren <simon.tenggren@gmail.com>
2025-03-08 10:56:20 +0100

    add stuff for working hot reload and hosting with vite

---
+++ src/Pages/Home.elm
@@ -0,0 +1,46 @@
+module Pages.Home exposing (main)
+
+import Browser
+import Ui.Nav
+import Html exposing (Html)
+
+type Msg
+  = NoOp
+
+type alias Model =
+  {
+  }
+
+type alias Flags =
+  {
+  }
+
+init: Flags -> (Model, Cmd Msg)
+init _ =
+  let
+    model: Model
+    model = {}
+  in
+  (model, Cmd.none)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update _ model = (model, Cmd.none)
+
+view: Model -> Browser.Document Msg
+view _ =
+  { title = "Simon Tenggren.xyz: Home Test Hello"
+  , body = 
+    [ (Ui.Nav.init { pages = [ "About", "CV", "Blog" ], selectedPage = Just "About" }
+        |> Ui.Nav.view )
+    , Html.text "Home"
+    ]
+  }
+
+main: Program Flags Model Msg
+main = Browser.document
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = (\_ -> Sub.none)
+    }
