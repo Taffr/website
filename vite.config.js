e8645fb7c8916c8693769be86322ed6ae3c5faed
Simon Tenggren <simon.tenggren@gmail.com>
2025-07-05 20:36:41 +0200

    Fix blog post resolution (#14)
    * wip
    
    * more blog post stuff
    
    * more on the if-statements blog
    
    * more implementation
    
    * update blog post

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import elmPlugin from 'vite-plugin-elm'
+import fg from 'fast-glob'
+
+const htmlFiles = fg.sync([
+  'index.html',
+  'cv.html',
+  'blog/**/*.html'
+]).reduce((entries, file) => {
+  // Preserve the nested path (minus the .html)
+  const name = file.replace(/\.html$/, '')
+  entries[name] = resolve(__dirname, file)
+  return entries
+}, {})
 
 export default defineConfig({
-  plugins:[ elmPlugin() ],
+  plugins: [ elmPlugin() ],
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        cv: resolve(__dirname, 'cv.html'),
-        blog: resolve(__dirname, 'blog/index.html'),
-      }
+      input: htmlFiles
     }
   }
 })
