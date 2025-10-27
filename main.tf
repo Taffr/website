42ec52e631ee4b4311c911c4a2a83d3e702ac81f
Simon Tenggren <simon.tenggren@gmail.com>
2025-10-27 09:04:59 +0100

    chore: release

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "google_cloud_run_v2_service" "default" {
 
   template {
     containers {
-      image = "europe-north1-docker.pkg.dev/simon-tenggren-website/website-repo/simon-tenggren-website:2025-10-26-22-26"
+      image = "europe-north1-docker.pkg.dev/simon-tenggren-website/website-repo/simon-tenggren-website:2025-10-27-09-02"
       ports {
         container_port = 80
       }
